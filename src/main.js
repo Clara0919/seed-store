864c74beee28cf97e7372b62c3c864846a5cc42c
Clara0919 <mansyuan.he@gmail.com>
2022-09-13 15:41:15 +0800

    配置axios、vue-axios並增加header解決接口的跨域問題 CORS

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// import ProductDetailView from './views/ProductDetailView.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+axios.defaults.baseURL = 'http://localhost:3000';
 
 const seed_app = createApp(App);
 // seed_app.component('ProductDetailView', ProductDetailView);
-seed_app.use(router).mount('#app');
+seed_app.use(router).use(VueAxios, axios).mount('#app');
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductDetailView from '../views/ProductDetailView.vue'
 import SelectedCategory from '../views/SelectedCategory.vue'
+import Vegetable from '../views/Vegetable.vue'
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     path: '/selected-category',
     name: 'SelectedCategory',
     component: SelectedCategory
+  },
+  {
+    path: '/vegetable',
+    name: 'vegetable',
+    component: Vegetable
   },
   {
     path: '/product-detail',
